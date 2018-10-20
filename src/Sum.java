ab7dcab83b4ba65eba5686694b677ab867d71bb6
Francesco Tucceri <francesco.tucceri@gmail.com>
2018-10-20 12:56:09 +0100

    Publisher.java

---
+++ src/Sum.java
@@ -0,0 +1,18 @@
+// Will it compile successfully? If yes, what;s the output of this java program?
+
+public class Sum {
+
+	public static void main(String... args) {
+
+		Double d1 = 12.8;
+		Float f1 = 12f;
+		int x = 10;
+
+		System.out.print("Sum:");
+		System.out.print();
+		System.out.println(d1 + f1 + x); 
+
+	}
+
+}
+
